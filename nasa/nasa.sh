92d6d58307cacc41b8f133b36eedab24dfc2a1fc
img <97775525+imgl@users.noreply.github.com>
2023-06-10 20:23:54 +0800

    Delete nasa.sh

--- nasa/nasa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if ! dpkg -s jq >/dev/null 2>&1; then
-  apt install jq -y 
-fi
-
-# 使用curl下载文件，并将结果存入变量
-json=$(curl -s -m 10 "https://api.nasa.gov/planetary/apod?api_key=1thfPMfPbM23lvZBCjg8FKVZQ4792tO18xRFZbfn") && \
-url=$(echo $json | jq -r '.url') && \
-curl -sSLO "$url"
